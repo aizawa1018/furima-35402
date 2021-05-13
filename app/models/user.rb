0464c368a1664fd038ac6506bbbd47a3a2431232
aizawa1018 <81944327+aizawa1018@users.noreply.github.com>
2021-05-13 18:21:13 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validetas :nickname,        presence: true
+         validetas :first_name,      presence: true
+         validetas :last_name,       presence: true
+         validetas :first_name_kana, presence: true
+         validetas :last_name_kana,  presence: true
+         validetas :birthday,        presence: true
+
+         has_many :items
+         has_many :order_historys
 end
