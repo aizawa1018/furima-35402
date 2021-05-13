220028d42d0a186813f43fb50964fe5112d8f94b
aizawa1018 <81944327+aizawa1018@users.noreply.github.com>
2021-05-13 18:59:06 +0900

    registrations/new.html.erbの編集をした

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validetas :nickname,        presence: true
-         validetas :first_name,      presence: true
-         validetas :last_name,       presence: true
-         validetas :first_name_kana, presence: true
-         validetas :last_name_kana,  presence: true
-         validetas :birthday,        presence: true
+         validates :nickname,        presence: true
+         validates :first_name,      presence: true
+         validates :last_name,       presence: true
+         validates :first_name_kana, presence: true
+         validates :last_name_kana,  presence: true
+         validates :birthday,        presence: true
 
-         has_many :items
-         has_many :order_historys
 end
