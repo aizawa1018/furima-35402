7c71bc7c238e9281c194e4b815c6b683e0ef9f07
aizawa1018 <81944327+aizawa1018@users.noreply.github.com>
2021-05-22 17:57:24 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :pride, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+
   belongs_to :category
   belongs_to :charge
   belongs_to :condition
   belongs_to :day
   belongs_to :Prefecture
   has_one_attached :image
+
+  
 
   with_options presence: true do
   validates :category_id
@@ -16,7 +21,10 @@ class Item < ApplicationRecord
   validates :title
   validates :description
   validates :pride
+  end
 
-  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :title
+    validates :description
   end
 end
