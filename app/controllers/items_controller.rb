109b0c843e7373833dc41851ee75bfd5ced817f4
aizawa1018 <81944327+aizawa1018@users.noreply.github.com>
2021-05-25 10:59:07 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:show]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -18,9 +18,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-    @items = Item.new
-  end
+  #def edit
+    #@items = Item.new
+  #end
 
   def show
   end
