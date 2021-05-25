7869bc957f04e4b38a44bfa45951df79d9c61c30
aizawa1018 <81944327+aizawa1018@users.noreply.github.com>
2021-05-25 15:50:03 +0900

    editを編集した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show,:edit,:update]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @items = Item.new(item_params)
+    @items = Item.new(items_param)
     if @items.save
       redirect_to root_path
     else
@@ -18,19 +18,29 @@ class ItemsController < ApplicationController
     end
   end
 
-  #def edit
-    #@items = Item.new
-  #end
+  def edit
+    redirect_to root_path unless @item.user_id == current_user.id
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+
 
   def show
   end
 
-  #def destroy
-   # if current_user.id == @item.user_id
-    #  @item.destroy
-     #end
-      #redirect_to root_path
-  #end
+  def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+     end
+      redirect_to root_path
+  end
 
   
 
@@ -38,8 +48,6 @@ private
 def item_params
   params.require(:item).permit(:user, :category_id, :fare_id, :condition_id,:days_id, :prefecture_id, :title, :description,:pride, :image).merge(user_id: current_user.id)
 end
-
-
 
 
 def set_item
